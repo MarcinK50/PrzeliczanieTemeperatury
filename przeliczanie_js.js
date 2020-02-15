1a38b827f379cfc673ba837eccc242a1cb08008c
Marrcin Kowalicki <marcinor02@gmail.com>
2020-02-15 22:26:10 +0100

    HTML: Poprawiono błędy JavaScript: Dodano wstępny zarys skryptu

---
+++ przeliczanie_js.js
@@ -0,0 +1,13 @@
+var pole;
+var wynik;
+
+function fahrenheit() {
+  pole = document.getElementById("wartosc").value;
+  wynik = pole * 1.8 + 32;
+
+  console.log(wynik);
+}
+
+function kelvin() {
+
+}
