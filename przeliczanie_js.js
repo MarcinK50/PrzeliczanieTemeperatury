76da263bff93844eb590ef24d427cc4d84885717
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2020-05-28 14:56:26 +0200

    Little changes

--- przeliczanie_js.js
+++ przeliczanie_js.js
@@ -3,27 +3,39 @@ var score = "";
 class Temperature {
 	constructor(value) {
 	this.value = value;
-	this.score_f_precision =  value -273.15 * 9 / 5 + 32;
-	this.score_f = this.score_f_precision.toFixed(2);
-	this.score_c = value * 1 - 273.15;
 	this.fahrenheit_score = "Wynik w Fahrenheitach: " + this.score_f + ".";
 	this.celcius_score = "Wynik w Celsjuszach: " + this.score_c + ".";
+	this.kelvin_score = "Wynik w Kelvinach: " + this.score_k + ".";
 	}
 	
-	fahrenheit_calc() {
-		return this.fahrenheit_score;
+	fahrenheit() {
+		this.score_f_precision =  (this.value - 273.15) * 9 / 5 + 32;
+		this.score_f = this.score_f_precision.toFixed(2);
+		return this.score_f;
 	}
 	
-	celcius_calc() {
+	celcius() {
+		this.score_c = (this.value - 273.15);
 		return this.celcius_score;
 	}
+	
+	kelvin() {
+		return this.kelvin_score;
+	}
+}
+
+function kelvin() {
+	let kelvin_value = document.getElementById("wartosc").value;
+	let kelvin = new Temperature(kelvin_value);
+
+	document.getElementById("wynik").innerHTML = kelvin.kelvin();
 }
 
 function fahrenheit() {
 	let fahrenheit_value = document.getElementById("wartosc").value;
-	let fahrenheit = new Temperature(fahrenheit_value)
+	let fahrenheit = new Temperature(fahrenheit_value);
 
-	document.getElementById("wynik").innerHTML = fahrenheit.fahrenheit_calc();
+	document.getElementById("wynik").innerHTML = fahrenheit.fahrenheit();
 }
 
 function kelvin() {
@@ -37,6 +49,5 @@ function celcius() {
 	let celcius_value = document.getElementById("wartosc").value;
 	let celcius = new Temperature(celcius_value)
 
-	document.getElementById("wynik").innerHTML = celcius.celcius_calc();
+	document.getElementById("wynik").innerHTML = celcius.celcius();
 }
-
